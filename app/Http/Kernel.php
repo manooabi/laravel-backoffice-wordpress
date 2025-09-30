418ecca5411a95d15ac4739eddfeb5aa65bae9eb
manooabi <manoo.abhillash@gmail.com>
2025-09-30 08:16:35 +0530

    Implementation of Middleware and register middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'wp.auth' => \App\Http\Middleware\WordPressAuth::class,
+
     ];
 }
